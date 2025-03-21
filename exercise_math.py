a9706166c8af10c28412b18a79e679f90c0f8e22
trinimaydana <tmaydana@mail.austral.edu.ar>
2025-03-21 10:57:10 -0300

    Update exercise_math.py

--- exercise_math.py
+++ exercise_math.py
@@ -1,3 +1,23 @@
 def math():
     a = 57
     b = 7
+    suma = a + b
+    print(suma)
+
+    resta = a - b
+    print(resta)
+
+    multiplicación = a*b
+    print(multiplicación)
+
+    promedio = (a + b)/ 2
+    print(promedio)
+
+    cociente_entero = a//b
+    print(cociente_entero)
+
+    resto_division = a % b
+    print(resto_division)
+
+    valor_real_division = a / b
+    print(valor_real_division)
